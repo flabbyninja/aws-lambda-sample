8fa3460ac7f92924eee6253647f4bf462af26fea
Flabby Ninja <dev@flabbyninja.net>
2019-08-07 15:08:29 +0100

    Add variable descriptions

--- tf/modules/ec2-apache/variables.tf
+++ tf/modules/ec2-apache/variables.tf
@@ -1,12 +1,14 @@
 variable "instance_count" {
+    description = "Number of EC2 instances to create"
     type = number
 }
 
 variable "user_data_script" {
+    description = "Location of EC2 user data script to install apache"
     type = string
 }
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type        = number
+  type = number
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,16 +1,19 @@
 variable "assume_user_role" {
+    description = "Name of the user to assume for Lambda execution"
     type = string
 }
 
 variable "instance_count" {
+    description = "Number of EC2 instances to create"
     type = number
 }
 
 variable "user_data_script" {
+    description = "Location of EC2 user data script"
     type = string
 }
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type        = number
+  type = number
 }
